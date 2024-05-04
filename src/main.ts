5c3d8cd78752309298f6811dc2f3de8c164eb6be
Roshinraj <roshinraj.thangasamy@finstein.ai>
2024-05-04 23:45:52 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true })
   const config = new DocumentBuilder()
-.setTitle('GigHub')
-.setDescription('CRUD Using NestJS and MySQL') .
-setVersion('1.0')
-.addTag('CRUD') .build();
-const document = SwaggerModule.createDocument(app, config); SwaggerModule.setup('gighub',
-app, document);
+    .setTitle('GigHub')
+    .setDescription('CRUD Using NestJS and MySQL').
+    setVersion('1.0')
+    .addTag('CRUD').build();
+  const document = SwaggerModule.createDocument(app, config); SwaggerModule.setup('gighub',
+    app, document);
   await app.listen(process.env.PORT);
 
 }
